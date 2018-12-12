aa957c2d2183a1952b1452b31232fd4f5783ad78
Elhiian <elhianp28@gmail.com>
2018-12-12 07:43:29 -0500

    wee

--- app/src/main/java/com/example/elhiian/turisappadsi/SplashActivity.java
+++ app/src/main/java/com/example/elhiian/turisappadsi/SplashActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -37,4 +38,24 @@ public class SplashActivity extends AppCompatActivity {
         },2000);
 
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==0){
+            if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+            else{
+                finish();
+            }
+
+        }
+    }
 }
+
+
